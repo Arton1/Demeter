7bf8467a7fef54e3c3c86c656f74bc963ec7c78b
Artur Wyrozębski <wyrozebski.artur@gmail.com>
2021-06-26 17:45:49 +0200

    NavBar

--- WebApplication/client-app/src/App.js
+++ WebApplication/client-app/src/App.js
@@ -1,14 +1,20 @@
 import {Switch, Route} from "react-router-dom";
+import "@progress/kendo-theme-default/dist/all.css"
+
 import Login from "./features/authentication/login/Login";
 import Home from "./app/Home"
 import Register from "./features/authentication/registration/Register"
+import HeaderBar from "./app/header/HeaderBar";
 
 function App() {
-  return (<Switch>
-    <Route path="/login" component={Login}/>
-    <Route path="/register" component={Register} />
-    <Route path="/" component={Home}/>
-  </Switch>);
+  return (<>
+      <HeaderBar/>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register} />
+        <Route path="/" component={Home}/>
+      </Switch>
+    </>);
 }
 
 export default App;
---
+++ WebApplication/client-app/src/app/header/HeaderBar.js
@@ -0,0 +1,37 @@
+﻿import {AppBar, AppBarSection, TabStrip, TabStripTab} from "@progress/kendo-react-layout";
+import {Link, NavLink} from "react-router-dom";
+import "./header.css";
+
+const buttons = [
+    {
+        name: "Login",
+        href: "/login"
+    },
+    {
+        name: "Rejestracja",
+        href: "/register"
+    }
+]
+
+function NavButtonList(props) {
+    return <ul className="navButtonList">
+        {props.buttons.map(({name, href}) => 
+            <li>
+                <NavLink className="listItem navLink clickable" to={href} activeClassName="selectedButton" exact>
+                    {name}
+                </NavLink>
+            </li>
+        )}
+    </ul>
+}
+
+export default function HeaderBar(props){
+    return <AppBar className="bar">
+        <AppBarSection>
+            <Link className="navLink clickable" to="/"><h2 className="title">Hello</h2></Link>
+        </AppBarSection>
+        <AppBarSection>
+            <NavButtonList buttons={buttons}/>
+        </AppBarSection>
+    </AppBar>;
+}
